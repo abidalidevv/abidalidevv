7872ce1fae20a933a28bea274006fe3986fdec1c
abidmmp <abidmmp99@gmail.com>
2017-08-02 10:00:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -1,3 +1,4 @@
 // Improve function signatures - 2024-07-22 10:12:00
 // Add helper methods - 2020-07-10 10:08:00
 // Add helper methods - 2017-05-06 10:04:00
+// Clean up unused variables - 2017-08-02 10:00:00
