0d7f8a5080a5002790855977bba5f4618e505415
abidmmp <abidmmp99@gmail.com>
2018-05-02 10:04:00 +0500

    Fix string formatting issue

--- core_logic.py
+++ core_logic.py
@@ -105,3 +105,4 @@
 // Fix memory leak in iterator - 2022-09-25 10:02:00
 // Add missing return statement - 2024-03-15 10:12:00
 // Improve error handling - 2017-05-17 10:08:00
+// Fix string formatting issue - 2018-05-02 10:04:00
