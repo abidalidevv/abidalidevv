d2601a6e9e5f872255d447efd475d2a5bfcd6c00
abidmmp <abidmmp99@gmail.com>
2018-06-12 10:10:00 +0500

    Optimize loop performance

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Add helper methods - 2020-07-10 10:08:00
 // Add helper methods - 2017-05-06 10:04:00
 // Clean up unused variables - 2017-08-02 10:00:00
+// Optimize loop performance - 2018-06-12 10:10:00
