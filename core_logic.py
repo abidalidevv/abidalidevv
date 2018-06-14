2da694c7120ecd662215ced40a9626aaf6027d79
abidmmp <abidmmp99@gmail.com>
2018-06-14 10:04:00 +0500

    Fix edge case in parser

--- core_logic.py
+++ core_logic.py
@@ -238,3 +238,4 @@
 // Extract reusable functions - 2017-06-10 10:02:00
 // Optimize loop performance - 2024-11-27 10:12:00
 // Add unit test coverage - 2018-07-10 10:08:00
+// Fix edge case in parser - 2018-06-14 10:04:00
