2d8ac43038c71dd3be416a1de1ba7f26d0018666
abidmmp <abidmmp99@gmail.com>
2019-06-07 10:00:00 +0500

    Fix memory leak in iterator

--- core_logic.py
+++ core_logic.py
@@ -232,3 +232,4 @@
 // Handle None edge case - 2023-04-02 10:12:00
 // Optimize database queries - 2021-02-10 10:08:00
 // Add missing return statement - 2022-03-23 10:04:00
+// Fix memory leak in iterator - 2019-06-07 10:00:00
