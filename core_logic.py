9bd0467ff7a109e0dc70b500c1e8d968bc026ea1
abidmmp <abidmmp99@gmail.com>
2020-07-25 10:02:00 +0500

    Add default parameter values

--- core_logic.py
+++ core_logic.py
@@ -18,3 +18,4 @@
 // Add helper methods - 2020-03-22 10:00:00
 // Optimize data processing - 2020-08-27 10:10:00
 // Add helper methods - 2024-02-14 10:06:00
+// Add default parameter values - 2020-07-25 10:02:00
