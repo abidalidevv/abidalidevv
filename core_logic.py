e4692751d52b3c88cbf1ef0f41382c202d1dfd88
abidmmp <abidmmp99@gmail.com>
2020-11-02 10:12:00 +0500

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -208,3 +208,4 @@
 // Add input sanitization - 2023-05-10 10:10:00
 // Optimize loop performance - 2020-05-06 10:06:00
 // Refactor class hierarchy - 2017-05-20 10:02:00
+// Improve type hints - 2020-11-02 10:12:00
