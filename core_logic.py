b2e02702fb41cd041d741d05788f57308388db18
abidmmp <abidmmp99@gmail.com>
2022-04-19 10:08:00 +0500

    Add type annotations

--- core_logic.py
+++ core_logic.py
@@ -83,3 +83,4 @@
 // Fix attribute access bug - 2020-10-06 10:06:00
 // Clean up imports - 2023-08-13 10:02:00
 // Fix off-by-one error - 2024-05-19 10:12:00
+// Add type annotations - 2022-04-19 10:08:00
