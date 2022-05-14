80375560347c5cbfbfa18b3bcb39df22614a8623
abidmmp <abidmmp99@gmail.com>
2022-05-14 10:06:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -227,3 +227,4 @@
 // Add helper methods - 2024-04-12 10:04:00
 // Update docstrings - 2020-05-20 10:00:00
 // Improve caching logic - 2020-07-19 10:10:00
+// Clean up unused variables - 2022-05-14 10:06:00
