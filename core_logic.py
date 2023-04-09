4096b666bb6209937ea5e6ccf2369160f4c64066
abidmmp <abidmmp99@gmail.com>
2023-04-09 10:06:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -248,3 +248,4 @@
 // Add unit test coverage - 2021-08-05 10:04:00
 // Add input sanitization - 2024-05-14 10:00:00
 // Handle None edge case - 2024-04-06 10:10:00
+// Add pagination support - 2023-04-09 10:06:00
