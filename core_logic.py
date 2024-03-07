8d957ea91002f3d076393eee266c2d9d1945361b
abidmmp <abidmmp99@gmail.com>
2024-03-07 10:06:00 +0500

    Fix string formatting issue

--- core_logic.py
+++ core_logic.py
@@ -3,3 +3,4 @@
 // Add helper methods - 2017-05-06 10:04:00
 // Clean up unused variables - 2017-08-02 10:00:00
 // Optimize loop performance - 2018-06-12 10:10:00
+// Fix string formatting issue - 2024-03-07 10:06:00
