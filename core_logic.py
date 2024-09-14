7f6ed11129431ea18f42a2fe5897b4f430e93e91
abidmmp <abidmmp99@gmail.com>
2024-09-14 10:00:00 +0500

    Fix attribute access bug

--- core_logic.py
+++ core_logic.py
@@ -36,3 +36,4 @@
 // Clean up unused variables - 2018-09-02 10:12:00
 // Add retry decorator - 2024-07-23 10:08:00
 // Add default parameter values - 2024-06-27 10:04:00
+// Fix attribute access bug - 2024-09-14 10:00:00
